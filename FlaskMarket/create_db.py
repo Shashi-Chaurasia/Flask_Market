2f75a377ec9c76d2ca5fb3e79d1e25ff0da9f8f6
Shashi_Chaurasia <shashichaurasia886@gmail.com>
2021-11-21 01:07:12 +0530

    models and databse

---
+++ FlaskMarket/create_db.py
@@ -0,0 +1,11 @@
+import mysql.connector
+mydb = mysql.connector.connect(host = 'localhost', user = 'root', passwd = 'rootpass',
+auth_plugin = 'mysql_native_password',)
+
+my_cursor = mydb.cursor()
+# my_cursor.execute("CREATE DATABASE user_market")
+
+my_cursor.execute("SHOW DATABASES")
+
+for db in my_cursor:
+    print(db)
